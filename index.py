76c297c2761d6e7dbe9f5983ac151038189270de
cdswpv <cdswpv@umsystem.edu>
2023-03-13 16:39:46 -0500

    Cleaned up files and added comments

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
+#Function used to start python file and iterate through for loop
 def main():
     for x in range (1, 101):
         FizzOrBuzz(x)
     return
 
+#Function used to determine if Fizz, Buzz, Fizzbuzz, or the number should be printed
 def FizzOrBuzz(num):
     if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz")
@@ -13,5 +15,5 @@ def FizzOrBuzz(num):
     else:
         print(num)
 
-
+#Calls function
 main()
